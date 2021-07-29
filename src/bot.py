04a3fe2d2d8b09d90c52afb3ed2f78c1f4d7ae5d
syno3 <sinotechxvista@gmail.com>
2021-07-29 22:09:26 +0300

    works fine

--- src/bot.py
+++ src/bot.py
@@ -7,27 +7,29 @@ try:
     import logging
     import time
     import json
-    from openai import ask, append_interaction_to_chat_log
+    from gpt import ask, append_interaction_to_chat_log
 ## checking for errors
 except ImportError:
     logging.warning('please run pip install -r requirements.txt')
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'etfjghgjyfjcgnfdkj'
 
 ## initializing the flask app
 @app.route('/bot', methods=['POST'])
 def bot():
     incoming_msg = request.values['Body']
+    resp = MessagingResponse()
+    msg = resp.message()
     chat_log = session.get('chat_log')
 
     answer = ask(incoming_msg, chat_log)
     session['chat_log'] = append_interaction_to_chat_log(incoming_msg, answer,
                                                          chat_log)
-
-    r = MessagingResponse()
-    r.message(answer)
-    return str(r)
+    ##resp.message(answer)
+    msg.body(answer)
+    
+    return str(resp)
 
 if __name__ == '__main__':
     app.run()
